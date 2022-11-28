6c0017de23800338f46141773cb544b2e843afab
arif1408 <maarifbatubara@gmail.com>
2022-11-28 13:40:44 +0700

    install npm
    npm

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+    <div class="w-full px-4">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 border-b border-gray-200">
+                You're logged in!
+            </div>
+        </div>
+    </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/inertia-vue3';
+
+export default{
+components:{
+BreezeAuthenticatedLayout,Head,
+},
+
+}
+</script>
